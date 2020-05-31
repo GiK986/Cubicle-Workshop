99fbc6462d7f045f783ec028cc6e191ebd07c960
GiK986 <g.kyosev86@gmail.com>
2020-05-31 19:51:39 +0300

    Setup view engine and routes

--- src/config/express.js
+++ src/config/express.js
@@ -1,13 +1,15 @@
-const express = require('express');
-const handlebars = require('express-handlebars');
-const bodyParser = require('body-parser');
+const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+const path = require("path");
 
 module.exports = (app) => {
-    
-    //TODO: Setup the view engine
+  //TODO: Setup the view engine
+  app.set("views", path.join(__dirname, "../views"));
+  app.engine(".hbs", handlebars({ extname: ".hbs" }));
+  app.set("view engine", ".hbs");
+  //TODO: Setup the body parser
 
-    //TODO: Setup the body parser
-
-    //TODO: Setup the static files
-
+  //TODO: Setup the static files
+  app.use(express.static(path.join(__dirname, "..", "static")));
 };
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || "development";
 
-const config = require('./config/config')[env];
-const app = require('express')();
+const config = require("./config/config")[env];
+const app = require("express")();
 
-require('./config/express')(app);
-require('./config/routes')(app);
+const homeRoute = require("./routes/home");
 
-app.listen(config.port, console.log(`Listening on port ${config.port}! Now its up to you...`));
+require("./config/express")(app);
+// require('./config/routes')(app);
+
+app.use("/", homeRoute);
+
+app.listen(
+  config.port,
+  console.log(`Listening on port ${config.port}! Now its up to you...`)
+);
