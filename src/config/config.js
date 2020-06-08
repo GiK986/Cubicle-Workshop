7c13878efd4faa11880910187e53c596064c887d
GiK986 <g.kyosev86@gmail.com>
2020-06-09 00:23:48 +0300

    feat: mongodb connection

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,10 @@
 const path = require("path");
+const { dbUser, dbPassword } = require("./config.json");
 
 module.exports = {
   development: {
     port: process.env.PORT || 3000,
+    databaseUrl: `mongodb+srv://${dbUser}:${dbPassword}@softuni-cxzmm.mongodb.net/cubicle`,
   },
   production: {},
 
--- src/index.js
+++ src/index.js
@@ -3,10 +3,21 @@ const env = process.env.NODE_ENV || "development";
 const config = require("./config/config")[env];
 const app = require("express")();
 const bodyParser = require("body-parser");
-
 const routes = require("./routes/routes");
 
 require("./config/express")(app);
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  config.databaseUrl,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  (error) => {
+    if (error) {
+      console.error(error);
+      return;
+    }
+  }
+);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
